dc94e5edff19c7d3368c446e972cc83ee414afc3
Artur Vale <47109035+ArturllVale@users.noreply.github.com>
2023-12-18 12:47:59 -0300

    Update Home

--- index.php
+++ index.php
@@ -10,7 +10,9 @@ include_once("config/dados.php");
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?php echo $title;?></title>
+    <title>
+        <?php echo $title; ?>
+    </title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="data/estilo.css">
@@ -21,9 +23,8 @@ include_once("config/dados.php");
         <?php include 'modules/header.php'; ?>
         <?php include 'modules/navbar.php'; ?>
         <?php include 'pages/home.php'; ?>
-        <?php include 'modules/footer.php'; ?>
     </div>
-
+    <?php include 'modules/footer.php'; ?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
