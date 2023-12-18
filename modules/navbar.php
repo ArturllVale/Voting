061e3aa16a57b88d6e7a4ab34f5cea15e742c231
Artur Vale <47109035+ArturllVale@users.noreply.github.com>
2023-12-18 12:25:56 -0300

    Update

--- modules/navbar.php
+++ modules/navbar.php
@@ -1,15 +1,15 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">Vote TOP</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse text-end" id="navbarNavAltMarkup">
       <div class="navbar-nav ms-auto">
-        <a class="nav-link active" aria-current="page" href="#">Home</a>
-        <a class="nav-link" href="#">Features</a>
-        <a class="nav-link" href="#">Pricing</a>
-        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        <a class="nav-link" href="#">Início</a>
+        <a class="nav-link" href="#">Sobre</a>
+        <a class="nav-link" href="#">Painel</a>
+        <a class="nav-link" href="#">Nova Conta</a>
       </div>
     </div>
   </div>
