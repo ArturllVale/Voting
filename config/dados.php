8e00c70d19cebf704ffc919a981fdc7910f4485a
Artur Vale <47109035+ArturllVale@users.noreply.github.com>
2023-12-18 12:42:03 -0300

    Update

---
+++ config/dados.php
@@ -0,0 +1,5 @@
+<?php 
+
+    $title  =   "Vote Agora";
+
+?>
--- index.php
+++ index.php
@@ -2,22 +2,32 @@
 session_start(); //Inicializar a sessão
 //Incluir a conexão com o banco de dados
 include_once("config/conexao.php");
+include_once("config/dados.php");
 ?>
 <!doctype html>
 <html lang="pt-br">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Top Voto</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-  </head>
-  <body>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="data/estilo.css">
+</head>
+
+<body>
     <div class="container-fluid">
-    <?php include 'modules/navbar.php'; ?>
-    <?php include 'modules/header.php'; ?>
-    <h1>Site de Voto</h1>
-    <?php include 'modules/footer.php'; ?>
+        <?php include 'modules/header.php'; ?>
+        <?php include 'modules/navbar.php'; ?>
+        <h1>Site de Voto</h1>
+        <?php include 'modules/footer.php'; ?>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-  </body>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
+</body>
+
 </html>
