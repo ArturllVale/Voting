1c04be133c6f7be8f4f5960a16d0e60054597520
Artur Vale <47109035+ArturllVale@users.noreply.github.com>
2023-12-18 12:13:23 -0300

    Update

--- index.php
+++ index.php
@@ -12,10 +12,10 @@ include_once("config/conexao.php");
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   </head>
   <body>
-    <?php include 'pages/navbar.php'; ?>
-    <?php include 'pages/header.php'; ?>
+    <?php include 'modules/navbar.php'; ?>
+    <?php include 'modules/header.php'; ?>
     <h1>Site de Voto</h1>
-    <?php include 'pages/footer.php'; ?>
+    <?php include 'modules/footer.php'; ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
   </body>
 </html>
