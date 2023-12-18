e9bc9dc5d79fc235e94de7eb1865921f1b2dad6d
Artur Vale <47109035+ArturllVale@users.noreply.github.com>
2023-12-18 10:56:10 -0300

    Config

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+<?php
+session_start(); //Inicializar a sessão
+//Incluir a conexão com o banco de dados
+include_once("config/conexao.php");
+?>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
