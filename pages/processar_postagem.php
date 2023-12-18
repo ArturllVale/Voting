631d39fc758b44bb9233fe68a197190bba5f3de6
Artur Vale <47109035+ArturllVale@users.noreply.github.com>
2023-12-18 15:30:21 -0300

    Update

--- pages/processar_postagem.php
+++ pages/processar_postagem.php
@@ -22,8 +22,17 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         // Inserir os dados no banco de dados
         $query_insert = "INSERT INTO posts (user_id, image_url, tag, text_content) VALUES (?, ?, ?, ?)";
         $stmt_insert = $conn->prepare($query_insert);
-        $default_image_url = ''; // Defina um valor padrão para image_url, já que não estamos fazendo upload
-        $stmt_insert->bind_param("isss", $user_id, $default_image_url, $tag, $text_content);
+
+        // Verificar se o campo da URL da imagem está definido
+        if (isset($_POST['image_url'])) {
+            $image_url = $_POST['image_url'];
+        } else {
+            // Caso o campo da URL da imagem não esteja definido, defina um valor padrão ou lide com a lógica apropriada
+            $image_url = ''; // Defina um valor padrão ou lógica apropriada
+        }
+
+        $stmt_insert->bind_param("isss", $user_id, $image_url, $tag, $text_content);
+
 
         if ($stmt_insert->execute()) {
             // Redirecionar de volta para a página de gerenciamento
